d8d9628dd35d214c18678b521648a86945f0651e
Mian_Danyal <danyalm@sunypoly.edu>
2023-02-07 17:35:17 -0500

    Committing objects

--- exercises03/src/main/java/edu/danyalm/exercises03/FoodRationer.java
+++ exercises03/src/main/java/edu/danyalm/exercises03/FoodRationer.java
@@ -1,20 +1,56 @@
 package edu.danyalm.exercises03;
 
-import java.util.Scanner;
+import java.util.*;
 public class FoodRationer {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        Supplies supplies = new Supplies();
+
 
         System.out.println("Welcome to the trail!");
 
         System.out.println("Enter total food:");
-        double totalFood = input.nextDouble();
+        supplies.addFood(input.nextDouble());
+
         System.out.println("Enter number of people: ");
         int partyCnt = input.nextInt();
         double rations = 10;
 
-        double daysLeft = totalFood/(partyCnt*rations);
+        double daysLeft = supplies.getTotalFood()/(partyCnt*rations);
         System.out.println("DAYS LEFT: " + daysLeft);
+        /*
+        double y = 2.0;
+        int x = 10;
+        x = (int)(x/y);
+
+        String t = null;
+
+        if(t != null)
+        {
+            System.out.println(t);
+            System.out.println(t.length());
+        }
+        String s = "";
+        System.out.println(s);
+        System.out.println(s.length());
+
+        char c = s.charAt(0);
+        */
+        Random r1 = new Random(42);
+        Random r2 = new Random(42);
+        System.out.println("RANDOM");
+        for(int i = 0; i < 5; i++)
+        {
+            System.out.println(r1.nextDouble());
+            System.out.println(r2.nextDouble());
+        }
+
+        boolean foundTreasure = true;
+
+        if(foundTreasure == true)
+        {
+            System.out.println("HUZZAH!");
+        }
     }
 }
